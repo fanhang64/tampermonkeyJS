f300a791f971b919121bde95f7a4a31d87bd1e55
fanhang64 <fanhang64@gmail.com>
2021-05-21 22:33:35 +0800

    修改了之前粗暴的删除逻辑

--- bing_search_results_filter.js
+++ bing_search_results_filter.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Bing_Search_Result_Filter
 // @author       fanhang64
-// @update      2021-03-26
-// @version      0.1
+// @update      2021-05-21
+// @version      0.2
 // @description  过滤必应搜索结果
 // @include     https://cn.bing.com/*
 // @grant        none
@@ -10,10 +10,10 @@
 //    'use strict';
 (function() {
     const blankList = "csdn|CSDN";
-    let iTems = blankList.split("|") 
+    let iTems = blankList.split("|")
     let iLength = iTems.length;
     document.body.addEventListener("DOMNodeInserted", removeitems);
-    
+
     function removeitems(){
         let sDom = document.getElementById("b_results")
         if (sDom == null) return;
@@ -29,7 +29,12 @@
     }
     function exec_remove(elems){
         for(let i=elems.length-1; i>=0; i--){
-            let stext = elems[i].innerText;
+            let stext = "";
+            let b_caption = elems[i].getElementsByClassName('b_caption');
+            if(!b_caption){
+                continue;
+            }
+            stext = b_caption[0].innerText;
             for(let k = 0; k < iLength; k++){
                 if(stext.includes(iTems[k]) === true){
                     if(elems[i] && elems[i].remove){
